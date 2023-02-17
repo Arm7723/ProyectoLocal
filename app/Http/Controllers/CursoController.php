c9ddc223ba2885b55a9588b3abfdc249f73e6b03
Arm7723 <tonachinchin@gmail.com>
2023-02-17 15:46:48 +0100

    validacion de formularios

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -19,6 +19,14 @@ class CursoController extends Controller
     }
 
     public function store(Request $request){
+
+       $request->validate([
+            'name' => 'required|max:10',
+            'descripcion' => 'required|min:10',
+            'categoria' => 'required'
+        ]); 
+
+
         $curso = new Curso();
 
         $curso->name = $request->name;
@@ -41,6 +49,14 @@ class CursoController extends Controller
     }
 
     public function update(Request $request, Curso $curso){
+
+        $request->validate([
+            'name' => 'required|max:10',
+            'descripcion' => 'required|min:10',
+            'categoria' => 'required'
+        ]);
+
+
         $curso->name = $request->name;
         $curso->descripcion = $request->descripcion;
         $curso->categoria = $request->categoria;
